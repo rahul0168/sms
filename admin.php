6786891e91c60ca9260e8087998d24338417da35
rahul0168 <rahul.naikmule.facebook@gmail.com>
2021-12-24 00:00:18 +0530

    4 commit

---
+++ admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>GCC</title>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"><!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+
+
+
+
+</head>
+<body>
+
+    <div style="background: rgb(0, 162, 255); padding: 15px; color: aliceblue;font-weight: 600;font-size: 20px;">Student Mangement Sytem</div>
+    <div class="container">
+   <div class="row">
+       <div class="text-center"><h1>Admin</h1> </div>
+   <div class="col-md-4">
+    <a href="add_country.php"><button class="btn btn-primary">Add Country</button></a><br>
+   
+    </div>
+    <div class="col-md-4">
+ 
+    <a href="add_course.php"><button class="btn btn-primary">Add Course</button></a><br>
+
+    </div>
+    <div class="col-md-4">
+ 
+    <a href="add_city.php"><button class="btn btn-primary">Add City</button></a><br>
+    </div>
+    </div>
+</body>
+</html>
